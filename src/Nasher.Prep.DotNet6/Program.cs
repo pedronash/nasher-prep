15c6d7865f38df9e8447a6d12649320f88b98276
Pete Nash <pete.nash@oakbrookfinance.com>
2023-03-02 09:02:27 +0000

    Move tests to folder.

--- src/Nasher.Prep.DotNet6/Program.cs
+++ src/Nasher.Prep.DotNet6/Program.cs
@@ -1,8 +1,12 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
